38c16727e5f907587d20841a47592f1ea93213de
Sahiru22 <sahirubalasuriya222@gmail.com>
2024-05-03 02:15:33 +0530

    category add

--- backend/src/main/java/lk/ITPM/cco2/controller/UserSubmitAnswerController.java
+++ backend/src/main/java/lk/ITPM/cco2/controller/UserSubmitAnswerController.java
@@ -3,10 +3,10 @@ package lk.ITPM.cco2.controller;
 import lk.ITPM.cco2.dto.request.UserSubmitAnswerRequest;
 import lk.ITPM.cco2.service.UserSubmitAnswerService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -17,5 +17,15 @@ public class UserSubmitAnswerController {
     @PostMapping("/questions/{question-id}/answers")
     public void create(@RequestBody UserSubmitAnswerRequest request, @PathVariable("question-id") String questionId){
         userSubmitAnswerService.create(request,questionId);
+    }
+
+    @GetMapping("/{name}/category-counts")
+    public List<String> getCorrectAnswerCountByCategory(@PathVariable String name) {
+        return userSubmitAnswerService.getCorrectAnswerCountByCategory(name);
+    }
+
+    @GetMapping("/{name}/false-answers-keywords")
+    public Map<String, List<String>> getFalseAnswersKeywordsByCategory(@PathVariable String name) {
+        return userSubmitAnswerService.getFalseAnswersKeywordsByCategory(name);
     }
 }
--- backend/src/main/java/lk/ITPM/cco2/repository/UserSubmitAnswerRepository.java
+++ backend/src/main/java/lk/ITPM/cco2/repository/UserSubmitAnswerRepository.java
@@ -3,5 +3,10 @@ package lk.ITPM.cco2.repository;
 import lk.ITPM.cco2.model.UserSubmitAnswer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface UserSubmitAnswerRepository extends MongoRepository<UserSubmitAnswer,String> {
+    List<UserSubmitAnswer> findByAnswerStatusTrue(String name);
+
+    List<UserSubmitAnswer> findByAnswerStatusFalse(String name);
 }
