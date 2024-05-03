c77a9700fe8a454087a56a9ebfff05ce0d22f451
Gunaratne <it21326936@my.sliit.lk>
2024-05-04 04:21:17 +0530

    quiz edit

--- frontend/src/pages/BeginnerQuiz/categoryView.jsx
+++ frontend/src/pages/BeginnerQuiz/categoryView.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import './categoryView.scss';
 
 const categoryView = () => {
   const [categories, setCategories] = useState([]);
--- frontend/src/pages/BeginnerQuiz/quizEdit.jsx
+++ frontend/src/pages/BeginnerQuiz/quizEdit.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './quizEdit.scss';
 import { useParams } from 'react-router-dom';
 
 const quizEdit = () => {
