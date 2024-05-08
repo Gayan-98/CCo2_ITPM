dc6c4ff616982c944a7a2823d8db3b38a6aad5f9
Isuru rangana <86506992+Isuru-rangana@users.noreply.github.com>
2024-05-08 09:18:13 +0530

    bug fixed update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -170,7 +170,7 @@ function App() {
       element: <Layout2 />,
       children: [
         {
-          path: "/quizEdit:id", 
+          path: "/quizEdit/:id", 
           element: <QuizEdit />,
         },
       ],
